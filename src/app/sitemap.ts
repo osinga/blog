9e0d402d24c4b1f6555adb4432e9e040f8b67e29
osinga <6756723+osinga@users.noreply.github.com>
2023-09-14 10:37:52 +0200

    Filter out the style guide post from the sitemap (#8)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,10 +7,12 @@ const sitemap = (): MetadataRoute.Sitemap => [
 		url: 'https://osinga.blog',
 		lastModified: new Date().toISOString().split('T').at(0),
 	},
-	...allPosts.map(post => ({
-		url: `https://osinga.blog/posts/${post.slug}`,
-		lastModified: (post.modified || post.published).split('T').at(0),
-	})),
+	...allPosts
+		.filter(post => process.env.NODE_ENV === 'production' ? post.slug !== 'style-guide' : true)
+		.map(post => ({
+			url: `https://osinga.blog/posts/${post.slug}`,
+			lastModified: (post.modified || post.published).split('T').at(0),
+		})),
 ]
 
 export default sitemap
