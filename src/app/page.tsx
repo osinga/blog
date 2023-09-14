eab6e0fceef4b3d5b0cfb2647323187536b61fa3
Osinga <6756723+osinga@users.noreply.github.com>
2023-09-14 22:15:25 +0200

    Tweak various things (#10)
    * Remove the mail icon from email links
    
    * Remove the email address from the feed
    
    * Tweak the byline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Page = () => (
 		<section className="prose">
 			<Paragraph variant="lead">Hello</Paragraph>
 			<Paragraph className="max-w-lg">
-				Product Manager and aspiring solopreneur.
+				Product manager, software developer, and aspiring solopreneur.<br />
 				Open to a new challenge.
 				Reach out to me on{` `}
 				<Link href="mailto:contact@osinga.blog">contact@osinga.blog</Link>.
