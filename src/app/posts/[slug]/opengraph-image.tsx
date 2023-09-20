af4fd0622849c12eea6ed695205dcf8f609d8c4c
osinga <6756723+osinga@users.noreply.github.com>
2023-09-20 21:18:48 +0200

    Wrap the post title and description

--- src/app/posts/[slug]/opengraph-image.tsx
+++ src/app/posts/[slug]/opengraph-image.tsx
@@ -28,12 +28,18 @@ const Image = ({ params }: ImageProps) => {
 			<h1 style={{
 				margin: 0,
 				fontSize: 96,
+				// @ts-expect-error Not in the types yet, but available as per
+				// https://twitter.com/shuding_/status/1703868006143528972
+				textWrap: 'balance',
 			}}>{post.title}</h1>
 
 			<p style={{
 				margin: 0,
 				fontSize: 48,
 				color: colors.neutral[500],
+				// @ts-expect-error Not in the types yet, but available as per
+				// https://twitter.com/shuding_/status/1703868006143528972
+				textWrap: 'balance',
 			}}>{post.description}</p>
 
 			<hr style={{
