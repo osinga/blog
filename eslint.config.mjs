5d3caaa1c24cc1d4be3f7cfeb9a90d7b005d7b06
osinga <6756723+osinga@users.noreply.github.com>
2025-03-23 13:36:28 +0100

    Tweak the configuration syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,15 @@
 import { FlatCompat } from '@eslint/eslintrc'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+	baseDirectory: import.meta.dirname,
 })
 
-const eslintConfig = [
-	// ...compat.extends('next/core-web-vitals', 'next/typescript'),
-	...compat.extends('next/core-web-vitals'),
+const config = [
+	...compat.config({
+		extends: [
+			'next/core-web-vitals',
+		],
+	}),
 ]
 
-export default eslintConfig
+export default config
